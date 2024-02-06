f54833e2982a8ba38eddaf8c7ffa2316ecd2e154
surya612000 <surya612000@gmail.com>
2024-02-06 11:27:51 +0530

    api for creating single user

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from fastapi import  FastAPI
+from fastapi import  FastAPI,Depends
 from database import  engine,get_db
 # from routers import Customer,Customer_refer,Product,Productvariations,Order,Orderitems,login
 import models
+from sqlalchemy.orm import Session
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -12,3 +13,8 @@ app=FastAPI()
 @app.get("/users")
 def get_user(id:int):
     return {"id":id}
+
+@app.get("/users/{id}")
+def create_user(id:int,db:Session=Depends(get_db)):
+    db_user=db.query(models.Student).filter(models.Student.id==id).first()
+    return db_user

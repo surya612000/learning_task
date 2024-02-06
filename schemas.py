1849febc30a2ada3c2b544f7d9689ab7f6372303
surya612000 <surya612000@gmail.com>
2024-02-06 12:34:45 +0530

    some new schemas are addded

--- schemas.py
+++ schemas.py
@@ -2,3 +2,14 @@ from pydantic import BaseModel
 
 class createUser(BaseModel):
     name:str
+
+class student_out(BaseModel):
+    id:int
+    name:str
+
+class Course_create(BaseModel):
+    name:str
+
+class Course_out(BaseModel):
+    id:int
+    name:str
